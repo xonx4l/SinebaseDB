21002396d8824ec0031040b7edd71a5d5602cccd
xonx4l <sk797119@gmail.com>
2025-05-30 23:35:48 +0530

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
+pub mod args;
+pub mod consensus;
 
+use actix_web::{App, HttpServer};
+use args::parse_args;
+use consensus::{init_consensus, run_consensus};
+use tokio::task;
+
+#[actix_web::get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main () -> std::io::Result<()> {
+    let args = parse_args();
+    println!("Starting node with url: {}", args.url);
+
+    task::spawn(async move {
+        let mut node  = init_consensus(&args)
+            .await
+            .expect("Failed to initialize consensus");
+        run_consensus(&mut node).await;    
+    });
+
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:9900")?
+        .run()
+        .await 
+}
