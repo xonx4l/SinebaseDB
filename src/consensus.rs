deeccdfb41a28d8879607055a7853c9771a9a4b3
xonx4l <sk797119@gmail.com>
2025-05-30 20:53:02 +0530

    add changes

--- src/consensus.rs
+++ src/consensus.rs
@@ -1,31 +1,77 @@
-   use std::{collections::Hashmap, sync::Arc};
-   use::tokio::sync::{mpsc, Mutex};
-   use raft::{
-    Config,
-    raw_node::RawNode,
-    storage::{MemStorage, Storage},
-    raftpb::{Entry, EntryType, Message},
-    SnapshotStatus,
+use crate::args::Args;
+use raft::{prelude::{Entry, EntryType, Message}, storage::MemStorage, Config, RawNode};
+use slog::{Drain, o};
+use std::{
+    collections::HashMap,
+    error::Error,
+    sync::mpsc::{RecvTimeoutError, channel},
+    time::Duration,
+};
+use tokio::time::Instant;
+
+const RAFT_TICK_INTERVAL: Duration = Duration::from_millis(100);
+
+pub async fn init_consensus(_args: &Args) -> Result<RawNode<MemStorage>, Box<dyn  Error>> {
+    let config = Config {
+        id: 1, 
+        ..Default::default()
     };
-   use slog::{info, o, Drain, Logger};
 
-   #[derive(Debug, Clone)]
-   pub enum Command {
-    Set {key:String , value:String}
-    Delete {key:String}
-   }
+    let logger = slog::Logger::root(slog_stdlog::Stdlog.fuse(), o!());
+    let storage = MemStorage::new_with_conf_state((vec![1], vec![]));
+    let node = RawNode::new(&config, storage, &logger)?;
 
-   impl Command {
-        pub fn serialize(&self) -> Vec<u8> {
-            match self {
-                command::Set { key, value } => format!("SET {} {}", key, value).into_bytes(),
-                Command::Delete {key} => format!("DEL {}", Key).into_bytes(),
-                
+    Ok(node)
+}
+
+enum Msg {
+    Propose {
+        id: u8,
+        callback: Box<dyn Fn() + Send>,
+    },
+    #[expect(dead_code)]
+    Raft(Message),
+}
+
+pub async fn run_consensus(node: &mut RawNode<MemStorage>) {
+    run_consensus_receiver_loop(node).await;
+    run_consensus_sender_loop(node).await;
+}
+
+async fn run_consensus_sender_loop(node: &mut RawNode<MemStorage>) {
+    let (tx, rx) = channel();
+    let mut remaining_timeout = RAFT_TICK_INTERVAL;
+
+    let _ = tx.send(Msg::Propose {
+        id:1,
+        callback: Box::new(|| ()),
+    });
+
+    let mut cbs = HashMap::new();
+    loop {
+        let now = Instant::now();
+
+        match rx.recv_timeout(remaining_timeout) {
+            Ok(Msg::Propose {id, callback }) => {
+                cbs.insert(id, callback);
+                let result = node.propose(vec![], vec![id]);
+                println!("Propose result: {:?}", result);
             }
-        } 
-
-        pub fn deserialize(data: &[u8]) -> Option<Self> {
-            let s = String::from_utf8_lossy(data);
-            
+            Ok(Msg::Raft(m)) => {
+                let result = node.step(m);
+                println!("Raft result: {:?}", result);
+            }
+            Err(RecvTimeoutError::Timeout) => (),
+            Err(RecvTimeoutError::Disconnected) => unimplemented!(),
         }
-   }
+
+        let elapsed = now.elapsed();
+        if elapsed >= remaining_timeout {
+            remaining_timeout = RAFT_TICK_INTERVAL;
+            node.tick();
+        } else {
+            remaining_timeout -= elapsed; 
+        }
+        break;
+    }
+}
